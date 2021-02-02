08ad9a9361cbfe97b103895e1c1da84e8ac70215
Zzeynepyilmaz <77531493+Zzeynepyilmaz@users.noreply.github.com>
2021-02-02 23:54:22 +0300

    ReCapProject

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,30 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            ColorManager colorManager = new ColorManager(new InMemoryColorDal());
+            BrandManager brandManager = new BrandManager(new InMemoryBrandDal());
+            foreach (var cars in carManager.GetAll())
+            {
+                Console.WriteLine(cars.ModelYear + " Model ," + cars.DailyPrice + " TL," + cars.Description);
+            }
+            Console.WriteLine("---------------");
+            foreach (var color in colorManager.GetAll())
+            {
+                Console.WriteLine(color.ColorName);
+            }
+            Console.WriteLine("---------------");
+            foreach (var brand in brandManager.GetAll())
+            {
+                Console.WriteLine(brand.BrandName);
+            }
+        }    
+    }
+}
